b84a090991f7e4103e4769c45b475fa443492c51
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-03-26 14:45:15 -0400

    started chapter 2

---
+++ 02_ProgramStructure/bindingsEtc.js
@@ -0,0 +1,34 @@
+// Examples of expressions
+1;
+!true;
+false && 'hello';
+33 - 10;
+
+// Examples of statmenets
+let hi = !true; // Let statement
+
+// multiple bindings (variables) can be defined in 1 statement.
+let one = 1,
+  two = 2;
+
+// Since functions are expressions, they can be used within larger expressions.
+console.log(Math.min(2, 4) + 100); // multiple bindings (variables) can be defined in 1 statement.
+
+// let theNumber = Number(prompt('Pick a number:')); // prompt function nested in Number, which converts into INT, just like String() and Boolean()
+// alert(theNumber);
+
+// Counts to 12
+let num = 0;
+while (num <= 12) {
+  console.log(num);
+  num++;
+}
+
+// Gets 2 to the 10th power
+let counter = 0;
+let result = 1;
+while (counter < 10) {
+  result = result * 2;
+  counter++;
+}
+console.log(result);
