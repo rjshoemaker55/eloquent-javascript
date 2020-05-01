6c11e5b4b7a2850485dc5a6d02f93e0c940b5109
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-05-01 13:59:45 -0400

    added slice and concat methods

--- 04_DataStructures/slice.js
+++ 04_DataStructures/slice.js
@@ -2,3 +2,17 @@
 let array = [1, 2, 3, 4, 5];
 
 console.log(array.slice(2, 4));
+
+// when an end index is not given, it just returns all elements after the first
+console.log(array.slice(2));
+
+// concat is used to combine multiple strings to create an array
+
+function remove(array2, index) {
+  return array2
+    .slice(0, index) // creates a new array starting from 0 and between 2 [ 'a', 'b' ]
+    .concat(array2.slice(index + 1)); // create a new array starting from 3 and going to end [ 'd', 'e' ]
+  // then combines (concatinates) with the first array ['a', 'b', 'd', 'e']
+}
+
+console.log(remove(['a', 'b', 'c', 'd', 'e'], 2));
