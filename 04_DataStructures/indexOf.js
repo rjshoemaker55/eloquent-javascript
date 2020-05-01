9261b9193a521d7b01d30f7949936899da8ae573
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-05-01 13:44:53 -0400

    index of

---
+++ 04_DataStructures/indexOf.js
@@ -0,0 +1,5 @@
+let array = [1, 2, 3, 2, 1];
+
+console.log(`Array: ${array}`);
+console.log(`indexOf(2): ${array.indexOf(2)}`);
+console.log(`lastIndexOf(2): ${array.lastIndexOf(2)}`);
---
+++ 04_DataStructures/slice.js
@@ -0,0 +1,4 @@
+// slice() takes a start and end index and returns an array of the elements in between
+let array = [1, 2, 3, 4, 5];
+
+console.log(array.slice(2, 4));
